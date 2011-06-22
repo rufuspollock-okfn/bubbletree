78a57013e19acd65c3a08358220a41eb63da92ee
gka <gka@vis4.net>
2011-06-22 11:59:02 +0200

    modified cra exmaple

--- examples/cra/config.js
+++ examples/cra/config.js
@@ -2,11 +2,11 @@ var config = {
 	// todo: we should really document all the settings
 	apiUrl: 'http://openspending.org/api',
 	// todo: ...
-	localApiCache: 'data/aggregate-cra.json',
+	//localApiCache: 'data/aggregate-cra.json',
 	// todo: we should really document all the settings		
 	dataset: 'cra',
 	// todo: we should really document all the settings
-	drilldowns: ['cofog1', 'cofog2', 'cofog3', 'from'],
+	drilldowns: ['cofog1', 'cofog2', 'region'],
 	// todo: we should really document all the settings
 	cuts: ['year:2009'],
 	// todo: we should really document all the settings
@@ -16,11 +16,11 @@ var config = {
 	// 
 	initYear: 2009,
 	// breakdown
-	breakdown: 'region',
+	breakdown: 'cg_lg_or_pc',
 	// this callback is invoked as soon as the year changes by url
 	// defines what class is used to render the bubbles
 	// possible values are pie,donut,plain,multi,icon
-	bubbleType: ['icon','icon','icon','plain'],
+	bubbleType: ['icon','icon','icon','donut'],
 	// fake breakdown values for each node
 	fakeBreakdowns: ['Government', 'General Bugdet Support', 'Aid'],
 };
